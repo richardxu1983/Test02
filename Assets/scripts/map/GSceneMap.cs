ca5b70f230b37ecfed2b2b1a91212713ad74b052
richardxu <2622619554@qq.com>
2017-08-23 12:39:50 +0800

    update

--- Assets/scripts/map/GSceneMap.cs
+++ Assets/scripts/map/GSceneMap.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class GSceneMap : UnitySingleton<GSceneMap>
 {
 
+    float mapWidth = 200.0f;
+    float mapLength = 200.0f;
 
     public void CreateMap()
     {
@@ -15,19 +17,19 @@ public class GSceneMap : UnitySingleton<GSceneMap>
         //_terraindata.alphamapResolution = 100;
         //_terraindata.baseMapResolution = 64;
         _terraindata.SetDetailResolution(32, 32);
-        _terraindata.size = new Vector3(32, 1, 32);
+        _terraindata.size = new Vector3(mapWidth, 1, mapLength);
 
         SplatPrototype[] terrainTexture = new SplatPrototype[2];
         terrainTexture[0] = new SplatPrototype();
         terrainTexture[0].texture = (Texture2D)Resources.Load("GroudTextures/Mud01");
-        terrainTexture[0].tileSize = new Vector2(2, 2);
+        terrainTexture[0].tileSize = new Vector2(4, 4);
         terrainTexture[1] = new SplatPrototype();
         terrainTexture[1].texture = (Texture2D)Resources.Load("GroudTextures/grass01");
-        terrainTexture[1].tileSize = new Vector2(2, 2);
+        terrainTexture[1].tileSize = new Vector2(4, 4);
         _terraindata.splatPrototypes = terrainTexture;
 
         terrain = Terrain.CreateTerrainGameObject(_terraindata);
-        terrain.transform.position = new Vector3(0,0,0);
+        terrain.transform.position = new Vector3(0-mapWidth/2, 0, 0-mapLength/2);
         Terrain t = terrain.GetComponent<Terrain>();
         t.materialType = Terrain.MaterialType.BuiltInLegacyDiffuse;
         t.Flush();
